cf3c808b45e5a5f26c1e7c86213b12ab2e107e81
MiLara8888 <MiLara8888@mail.ru>
2022-05-05 19:26:09 +0300

    Categories added

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', show_news, name='news'),
-    path('<slug:post_slug>/', post_detail, name='post_detail'),
-    path('<slug:category_slug>/',category_list , name='category_url'),
-
+    path('<slug:category_slug>/', category_list, name='category_url'),
+    path('post/<slug:post_slug>/', post_detail, name='post_detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,23 +7,47 @@ from django.views.generic import TemplateView
 
 def post_detail(request, post_slug):
     post = get_object_or_404(Post, slug=post_slug)
-    category = Category_post.objects.all()
+    category_list = Category_post.objects.all()
     photo = Photo.objects.all()
     context = {'post': post,
-               'category': category,
-               'photo': photo, }
+               'photo': photo,
+               'category_list': category_list}
     return render(request, 'blog/post_detail.html', context=context)
 
 
-def category_list(request, category_slug):
-    category = get_object_or_404(Category_post, slug=category_slug)
-    context = {'category': category}
-    return render(request, 'blog/list_categories.html', context=context)
+# def category_list(request, category_slug):
+#     category = get_object_or_404(Category_post, slug=category_slug)
+#     context = {'category': category}
+#     return render(request, 'blog/list_categories.html', context=context)
 
+
+def category_list(request, category_slug=None):
+    category = None
+    posts = Post.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category_post, slug=category_slug)
+        posts = posts.filter(category_post=category)
+    if 'page' in request.GET:
+        page = request.GET['page']
+    else:
+        page = 1
+    paginator = Paginator(posts, 6)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    category_list = Category_post.objects.all()
+    context = {
+        'posts': posts,
+        'category': category,
+        'category_list': category_list
+    }
+    return render(request, 'blog/show_news.html', context)
 
 
 def show_news(request):
-
     posts = Post.objects.all().order_by('-time_create')
     if 'page' in request.GET:
         page = request.GET['page']
@@ -36,23 +60,9 @@ def show_news(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    category = Category_post.objects.all()
+    category_list = Category_post.objects.all()
     context = {
         'posts': posts,
-        'category':category
+        'category_list': category_list
     }
     return render(request, 'blog/show_news.html', context)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
